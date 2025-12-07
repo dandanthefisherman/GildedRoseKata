e58e0471b39a123192096d1d43d37b9f70f21870
dan.ryan <dan.ryan@betfred.com>
2025-12-07 10:46:55 +0000

    new logic to replace legacy code added. Contract added to the domain layer. Service added to the application layer which will be accessed by the program file. Updaters added to update specific item types. Factory added to update item based on which item type is send to it.

---
+++ src/GildedRose.Application/Services/ItemUpdater.cs
@@ -0,0 +1,17 @@
+﻿using GildedRose.Application.Factories;
+using GildedRose.Domain.Contracts;
+using GildedRose.Domain.Entities;
+
+namespace GildedRose.Application.Services;
+
+public class ItemUpdater 
+{
+    public void Update(List<Item> items)
+    {
+        foreach (var item in items)
+        {
+            UpdatableItem updater = ItemUpdaterFactory.Update(item);
+            updater.Update();
+        }
+    }
+}
